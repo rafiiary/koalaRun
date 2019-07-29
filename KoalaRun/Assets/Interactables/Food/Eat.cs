a3e30f8a19ab7c9f6303860409bc6b4d7c612841
rafiiary <arya.rafii@mail.utoronto.ca>
2019-07-29 10:30:38 -0400

    Added the food but the collider doesn't work

---
+++ KoalaRun/Assets/Interactables/Food/Eat.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eat : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("We have collided with the player");
+            Destroy(this);
+        }
+    }
+}
