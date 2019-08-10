c542aac590fe3ce1bf30b5e66b603ed2be465c16
rafiiary <arya.rafii@mail.utoronto.ca>
2019-08-10 16:08:00 -0400

    Added the leaf panel to the tutorial

--- KoalaRun/Assets/Tutorial/Scripts/TutorialCanvasController.cs
+++ KoalaRun/Assets/Tutorial/Scripts/TutorialCanvasController.cs
@@ -7,11 +7,15 @@ public class TutorialCanvasController : MonoBehaviour
     public Transform player;
     public GameObject MovementPanel;
     public GameObject jumpPanel;
+    public GameObject leafPanel;
+
+    bool leafPanelShown;
     private void Start()
     {
         //Movement panel should be visible from the start
         MovementPanel.SetActive(true);
         GameController.pauseGame();
+        leafPanelShown = false;
     }
     void Update()
     {
@@ -23,13 +27,23 @@ public class TutorialCanvasController : MonoBehaviour
         }
         if(jumpPanel != null && player.position.x > 0)
         {
-            Debug.Log("Player at position x=0");
             GameController.pauseGame();
             jumpPanel.SetActive(true);
         }
         if(jumpPanel != null && Input.GetKeyDown(KeyCode.Space))
         {
             Destroy(jumpPanel);
+            GameController.resumeGame();
+        }
+        if(leafPanel != null && player.position.x > 6)
+        {
+            GameController.pauseGame();
+            leafPanelShown = true;
+            leafPanel.SetActive(true);
+        }
+        if(leafPanel != null && leafPanelShown && Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(leafPanel);
             GameController.resumeGame();
         }
         
