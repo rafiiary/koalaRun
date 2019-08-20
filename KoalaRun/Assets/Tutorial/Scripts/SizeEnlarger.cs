e37f1f0266d9c677268a68f77a24d5405fbc1365
rafiiary <arya.rafii@mail.utoronto.ca>
2019-08-20 12:34:06 -0400

    Removed the leaf picture from tutorial and changed some positions of the texts

---
+++ KoalaRun/Assets/Tutorial/Scripts/SizeEnlarger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeEnlarger : MonoBehaviour
+{
+    public GameObject image;
+    public int initialScale;
+    public int targetScale;
+
+    Vector3 imageScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        imageScale = image.GetComponent<Transform>().localScale;
+        imageScale = new Vector3(1, 1) * initialScale;
+        StartCoroutine(Enlarge());
+    }
+    private IEnumerator Enlarge()
+    {
+        while(imageScale.x < targetScale)
+        {
+            imageScale += new Vector3(0.1f, 0.1f);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
